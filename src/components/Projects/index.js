3a88d937acaa162d7015f78afd553185e2568c1a
lequanos <lequanos@hotmail.com>
2021-07-25 13:53:13 +0200

    changement valeur défilement horizontal onTouch a 66

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -112,7 +112,7 @@ const Projects = ({
     if (xValue > xTouchValue) {
       setSliderValue((prevValue) => {
         if (prevValue < 100) {
-          return prevValue + 10000;
+          return prevValue + 66;
         }
         return prevValue;
       });
@@ -120,7 +120,7 @@ const Projects = ({
     else if (xValue < xTouchValue) {
       setSliderValue((prevValue) => {
         if (prevValue > 0) {
-          return prevValue - 10000;
+          return prevValue - 66;
         }
         return prevValue;
       });
